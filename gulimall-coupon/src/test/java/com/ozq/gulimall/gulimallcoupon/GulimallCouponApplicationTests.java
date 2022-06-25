bc43c82849063b09ab79550c8b76f1de0c439f0f
ozqcat <1204606415@qq.com>
2022-06-25 16:55:30 +0800

    2022-6-25 初始化项目

---
+++ gulimall-coupon/src/test/java/com/ozq/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozq.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
